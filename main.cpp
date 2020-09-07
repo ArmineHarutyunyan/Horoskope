7a0324612cf836f80c7378726008a98aca20d01a
ArmineHarutyunyan <49559690+ArmineHarutyunyan@users.noreply.github.com>
2020-09-07 20:28:53 +0400

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include "Horoskope.h"
+
+using namespace goroskop;
+
+int main()
+{
+    int n{};
+    std::cin >> n;
+    std::vector<Person> group;
+    for(int i = 0; i < n; ++i)
+    {
+        std::string fname{};
+        std::string lname{};
+        int y{};
+        int m{};
+        int d{};
+        std::cin >> fname >> lname >> y >> m >> d;
+        group.push_back(Person(fname, lname, y, m, d));
+    }
+    for(size_t i = 0; i < group.size(); ++i)
+    {
+        group[i].findChinZodiac();
+        group[i].findZodiac();
+        std::cout << "\n";
+    }
+}
